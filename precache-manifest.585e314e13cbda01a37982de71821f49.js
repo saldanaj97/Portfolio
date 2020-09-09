e543029e732ea99a3b410f993167c62f227a7e2d
Juan Saldana <saldanaj97@gmail.com>
2020-09-08 23:05:05 -0500

    Updates

---
+++ precache-manifest.585e314e13cbda01a37982de71821f49.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dae3b7e572340e8d05d2a8a2991d9ed",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "aedbd1ec97266d1f8940",
+    "url": "/Portfolio/static/css/3.f6606024.chunk.css"
+  },
+  {
+    "revision": "0795b6711653d502b869",
+    "url": "/Portfolio/static/css/4.4f17f8d6.chunk.css"
+  },
+  {
+    "revision": "7aee25b19f92e90b98fd",
+    "url": "/Portfolio/static/css/main.8e5a7c6f.chunk.css"
+  },
+  {
+    "revision": "690a32f54e85bd41ded7",
+    "url": "/Portfolio/static/js/2.e6b1aea6.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Portfolio/static/js/2.e6b1aea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aedbd1ec97266d1f8940",
+    "url": "/Portfolio/static/js/3.55943a50.chunk.js"
+  },
+  {
+    "revision": "0795b6711653d502b869",
+    "url": "/Portfolio/static/js/4.0047348f.chunk.js"
+  },
+  {
+    "revision": "7aee25b19f92e90b98fd",
+    "url": "/Portfolio/static/js/main.e1bbcb94.chunk.js"
+  },
+  {
+    "revision": "fea556e8857eb5bc068f",
+    "url": "/Portfolio/static/js/runtime-main.87a40b7e.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/Portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "0b849c72f38362fe12072a4916660013",
+    "url": "/Portfolio/static/media/cplusplus.0b849c72.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "ee1369e96e441ad0119530d10264b8b4",
+    "url": "/Portfolio/static/media/git.ee1369e9.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Portfolio/static/media/skill.34409398.svg"
+  }
+]);
