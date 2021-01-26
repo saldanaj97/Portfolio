3b5653c2656ed1c8fd97adec8b776fe9810fee34
Juan Saldana <saldanaj97@gmail.com>
2021-01-26 16:25:15 -0600

    Update Projects.js

--- swe_portfolio/src/containers/projects/Projects.js
+++ swe_portfolio/src/containers/projects/Projects.js
@@ -15,13 +15,12 @@ export default function Projects() {
 
   useEffect(() => {
     function getRepoData() {
-        console.log(openSource.githubConvertedToken);
         const client = new ApolloClient({
           uri: "https://api.github.com/graphql",
           request: (operation) => {
             operation.setContext({
               headers: {
-                authorization: '${openSource.githubConvertedToken}',
+                authorization: openSource.githubConvertedToken,
               },
             });
           },
