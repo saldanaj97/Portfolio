27146695a0005343fce74f8b742c020676073730
Juan Saldana <saldanaj97@gmail.com>
2021-03-10 17:43:37 -0600

    Updated file locations

---
+++ swe_portfolio 5.41.45 PM/src/components/achievementCard/AchievementCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./achievementcard.css";
+
+export default function AchivementCard({ cardInfo }) {
+  return (
+      <div className="certificate-card">
+        <div className="certificate-image-div">
+          <img src={cardInfo.image} alt="PWA" className="card-image"></img>
+        </div>
+        <div className="certificate-detail-div">
+          <h5 className="card-title">{cardInfo.title}</h5>
+          <p className="card-subtitle">{cardInfo.description}</p>
+        </div>
+      </div>
+  );
+}
