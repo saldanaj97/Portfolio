850099ccdaf979ffa663fd1006d4a87febf7c827
Juan Saldana <saldanaj97@gmail.com>
2021-03-11 11:23:48 -0600

    Updates

---
+++ precache-manifest.f4fc2c733e82f83591efad4ac881addc.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75048d674c38dda47e1e65d8bcd41679",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "15fba7cee8249aa33fc7",
+    "url": "/Portfolio/static/css/3.f6606024.chunk.css"
+  },
+  {
+    "revision": "df5eca2204cfcf63eb18",
+    "url": "/Portfolio/static/css/4.de2537e6.chunk.css"
+  },
+  {
+    "revision": "7eead7ffa72cb46430c6",
+    "url": "/Portfolio/static/css/main.81d0fa38.chunk.css"
+  },
+  {
+    "revision": "9c4dae7871ee894ab26f",
+    "url": "/Portfolio/static/js/2.b822d384.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/Portfolio/static/js/2.b822d384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15fba7cee8249aa33fc7",
+    "url": "/Portfolio/static/js/3.7a6154a9.chunk.js"
+  },
+  {
+    "revision": "df5eca2204cfcf63eb18",
+    "url": "/Portfolio/static/js/4.2686642e.chunk.js"
+  },
+  {
+    "revision": "7eead7ffa72cb46430c6",
+    "url": "/Portfolio/static/js/main.8f47e376.chunk.js"
+  },
+  {
+    "revision": "ba258beb40cfbce674aa",
+    "url": "/Portfolio/static/js/runtime-main.447d638b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/Portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "0b849c72f38362fe12072a4916660013",
+    "url": "/Portfolio/static/media/cplusplus.0b849c72.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "ee1369e96e441ad0119530d10264b8b4",
+    "url": "/Portfolio/static/media/git.ee1369e9.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "dc199ecc9e90e4bc7e9ed9b31e3c2b5f",
+    "url": "/Portfolio/static/media/python.dc199ecc.png"
+  },
+  {
+    "revision": "173e59faf265bf1b53cf2efb8b6bb571",
+    "url": "/Portfolio/static/media/wave-bottom.173e59fa.svg"
+  },
+  {
+    "revision": "44b09eca15bd9f08978a4336be39aec1",
+    "url": "/Portfolio/static/media/wave_repo_card.44b09eca.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b592b7a7dd5044e6294dce651d2243d4.js"
+  "/Portfolio/precache-manifest.f4fc2c733e82f83591efad4ac881addc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
