e8f87a3cf63fc11379624e04d490c85553fd76bc
Juan Saldana <saldanaj97@gmail.com>
2021-03-11 20:07:23 -0600

    Added in new github api requests

--- src/components/githubRepoCard/GithubRepoCard.js
+++ src/components/githubRepoCard/GithubRepoCard.js
@@ -11,7 +11,7 @@ export default function GithubRepoCard({ repo }) {
   return (
     <Fade bottom duration={1000} distance="20px">
     <div>
-      <div className="repo-card-div" key={repo.node.id} onClick={() => openRepoinNewTab(repo.node.url)}>
+      <div className="repo-card-div" key={repo.id} onClick={() => openRepoinNewTab(repo.url)}>
         <div className="repo-name-div">
           <svg aria-hidden="true"  height="20" role="img" viewBox="0 0 12 16" width="14" className="repo-svg">
             <path
@@ -19,18 +19,17 @@ export default function GithubRepoCard({ repo }) {
               d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"
             ></path>
           </svg>
-          <p className="repo-name">{repo.node.name}</p>
+          <p className="repo-name">{repo.name}</p>
         </div>
-        <p className="repo-description">{repo.node.description}</p>
+        <p className="repo-description">{repo.description}</p>
         <div className="repo-stats">
           <div className="repo-left-stat">
             <span>
-              <div className="language-color" style={{ backgroundColor: repo.node.primaryLanguage.color }}></div>
-              <p>{repo.node.primaryLanguage.name}</p>
+              <p>{repo.language}</p>
             </span>
           </div>
           <div className="repo-right-stat">
-            <p>{repo.node.diskUsage} KB</p>
+            <p>{repo.size} KB</p>
           </div>
         </div>
       </div>
