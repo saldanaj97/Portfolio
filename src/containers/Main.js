fd2e855422e97b518f386a785cb8e7050bcababe
Juan Saldana <saldanaj97@gmail.com>
2022-09-08 20:33:42 -0500

    Fixed spacing issues

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import Header from './header/Header';
-import Greeting from './greeting/Greeting';
-import Skills from './skills/Skills';
-import SkillProgress from './skillprogress/SkillProgress';
-import Projects from './projects/Projects';
-import Achievement from './achievement/Achievement';
-import Profile from './profile/Profile';
-import Top from './topbutton/Top';
+import React, { Component } from "react";
+import Header from "./header/Header";
+import Greeting from "./greeting/Greeting";
+import Skills from "./skills/Skills";
+import SkillProgress from "./skillprogress/SkillProgress";
+import Projects from "./projects/Projects";
+import Achievement from "./achievement/Achievement";
+import Profile from "./profile/Profile";
+import Top from "./topbutton/Top";
 
 export default class Main extends Component {
-    render(){
-        return (
-            <div>
-                <Header />
-                <Greeting />
-                <Skills />
-                <SkillProgress />
-                <Projects />
-                <Achievement />
-                <Profile />
-                <Top />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Greeting />
+        <Skills />
+        <SkillProgress />
+        <Projects />
+        <Achievement />
+        <Profile />
+        <Top />
+      </div>
+    );
+  }
 }
--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect ,lazy, Suspense } from "react";
+import React, { useState, useEffect, lazy, Suspense } from "react";
 import ApolloClient, { gql } from "apollo-boost";
 import { openSource } from "../../portfolio";
 import Contact from "../contact/Contact";
 import Loading from "../loading/Loading";
 
 const renderLoader = () => <Loading />;
-const GithubProfileCard = lazy(() => import('../../components/githubProfileCard/GithubProfileCard'));
+const GithubProfileCard = lazy(() => import("../../components/githubProfileCard/GithubProfileCard"));
 export default function Profile() {
   const [prof, setrepo] = useState([]);
   function setProfileFunction(array) {
@@ -40,24 +40,24 @@ export default function Profile() {
         setProfileFunction(result.data.user);
       })
       .catch(function (error) {
-          console.log(error);
-          setProfileFunction("Error");
-          console.log("Because of this Error Contact Section is Showed instead of Profile");
-          openSource.showGithubProfile = "false";
+        console.log(error);
+        setProfileFunction("Error");
+        console.log("Because of this Error Contact Section is Showed instead of Profile");
+        openSource.showGithubProfile = "false";
       });
-      console.log('Printing test token: ' + process.env.TEST_TOKEN);
+    console.log("Printing test token: " + process.env.TEST_TOKEN);
   }
   useEffect(() => {
     if (openSource.showGithubProfile === "true") {
       getProfileData();
     }
   }, []);
-if (openSource.showGithubProfile === "true" && !(typeof prof === 'string' || prof instanceof String)){  
+  if (openSource.showGithubProfile === "true" && !(typeof prof === "string" || prof instanceof String)) {
     return (
       <Suspense fallback={renderLoader()}>
-        <GithubProfileCard prof={prof} key={prof.id} /> 
+        <GithubProfileCard prof={prof} key={prof.id} />
       </Suspense>
-       );
+    );
   } else {
     return <Contact />;
   }
