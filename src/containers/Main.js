c56719b64d77a4678a8d165c2aee8dcdc06ea919
Juan Saldana <saldanaj97@gmail.com>
2023-04-02 15:26:53 -0500

    Adjusted styling for the project cards

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Header from "./header/Header";
 import Greeting from "./greeting/Greeting";
-import AboutMe from "./skills/AboutMe";
+import AboutMe from "./aboutme/AboutMe";
 import Projects from "./projects/Projects";
 import Profile from "./profile/Profile";
 import Top from "./topbutton/Top";
